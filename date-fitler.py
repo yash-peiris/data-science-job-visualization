4ccd922148978388abd14f2c58123584972fea75
Yasheen Peiris <yasheenmpeiris@gmail.com>
2021-10-05 09:47:59 +1100

    Added Line Chart

---
+++ date-fitler.py
@@ -0,0 +1,11 @@
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+
+df = pd.read_csv("data\listings2019_2021.csv")
+df['Jobs'] = 1
+
+df_date = df[['Date','Jobs']]
+df_date = df_date.groupby(['Date'],as_index = False)['Jobs'].sum()
+print(df_date)
+
+df_date.to_csv('Jobs-per-date.csv', index=False)
