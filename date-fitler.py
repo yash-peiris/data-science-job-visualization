ef2e41b844247100eb7e114ee7e7d07b8bfbb49f
Yasheen Peiris <yasheenmpeiris@gmail.com>
2021-10-09 08:44:36 +1100

    Updae jobs per date state csv

--- date-fitler.py
+++ date-fitler.py
@@ -13,11 +13,11 @@
 import pandas as pd
 pd.options.mode.chained_assignment = None  # default='warn'
 
-df = pd.read_csv("data\State-Time-Breakdown.csv")
+df = pd.read_csv("data\Jobs-per-date-state.csv")
 df['Jobs'] = 1
 
-df_date = df[['Date','state','Jobs']]
-# df_date = df_date.groupby(['Date'],as_index = False)['Jobs'].sum()
+df_date = df[['Year','state','Jobs']]
+df_date = df_date.groupby(['Year','state'],as_index = False)['Jobs'].sum()
 print(df_date)
 
 df_date.to_csv('Jobs-per-date-state.csv', index=False)
