a253d3a54f8ed493f15f48ace0ea816ce6264363
Yasheen Peiris <yasheenmpeiris@gmail.com>
2021-10-10 19:06:38 +1100

    Added job per class and sub class csv

--- date-fitler.py
+++ date-fitler.py
@@ -13,11 +13,11 @@
 import pandas as pd
 pd.options.mode.chained_assignment = None  # default='warn'
 
-df = pd.read_csv("data\Vic-data.csv")
+df = pd.read_csv("data\listings2019_2021.csv")
 df['Jobs'] = 1
 
-df_date = df[['listingYear','suburb','Jobs']]
-df_date = df_date.groupby(['listingYear','suburb'],as_index = False)['Jobs'].sum()
+df_date = df[['jobSubClassification','Jobs']]
+df_date = df_date.groupby(['jobSubClassification'],as_index = False)['Jobs'].sum()
 print(df_date)
 
-df_date.to_csv('Jobs-per-date-suburb.csv', index=False)
+df_date.to_csv('Jobs-per-sub-class.csv', index=False)
